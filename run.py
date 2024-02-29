ed81ab70c05cee490e2ead251fd92ed1d2f6af86
Madu-J Omenka <madu.omenka@hotmail.com>
2024-02-29 16:48:15 +0000

    Update game function

--- run.py
+++ run.py
@@ -9,12 +9,26 @@ print()
 if player.lower() != "yes":
     quit()
 else:
-    print("Here is the rules: Rock wins against scissors; paper wins against rock; and scissors wins against paper.")
+    print("Here is the rules: The computer will pick a random choice, but in normal contex the rock wins against scissors, paper wins against rock, and scissors wins against paper.")
 choose = ("rock", "paper", "scissors")
 player = None
 computer = random.choice(choose)
 
-player = input("Make a choice (rock, paper, scissors): ")
+while player not in choose:
+    player = input("Make a choice (rock, paper, scissors): ")
 
 print(f"Player: {player}")
 print(f"Computer: {computer}")
+
+if player == computer:
+    print("It's a tie ! ")
+elif player == "rock" and computer == "scissors":
+    print("You win ! ")
+elif player == "paper" and computer == "rock":
+    print("You win ! ")
+elif player == "scissors" and computer == "paper":
+    print("You win ! ")
+else:
+    print("You lose ! ")
+
+print("Thanks for playing ! ")
