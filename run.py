e0a35751109a30703e0ae9e85b00782ba56575bc
Madu-J Omenka <madu.omenka@hotmail.com>
2024-02-29 00:19:45 +0000

    Add game rules

--- run.py
+++ run.py
@@ -2,14 +2,14 @@
 import random
 
 print("Welcome to Game Box!")
-
+# Player chooses whether to play or quit
 player = input("Do you want to play Rock, Paper, Scissors Game?")
 print()
 
 if player.lower() != "yes":
     quit()
 else:
-    print("Here is the rules")
+    print("Here is the rules: Rock wins against scissors; paper wins against rock; and scissors wins against paper.")
 choose = ("rock", "paper", "scissors")
 player = None
 computer = random.choice(choose)
